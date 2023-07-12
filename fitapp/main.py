d0ec482ff330eef9550ec9c9a2d6fae8dec646ff
Kirill Zh <kirillzivushko@gmail.com>
2023-07-12 12:09:30 +0300

    fit2

--- fitapp/main.py
+++ fitapp/main.py
@@ -1,1 +1,8 @@
-import kivymd
+from kivymd.app import MDApp 
+from kivymd.uix.button import MDTextButton
+from kivymd.uix.label import MDLabel
+class MyApp(MDApp):
+    def build(self):
+        return MDLabel(text='Hello')
+app = MyApp()
+app.run()
